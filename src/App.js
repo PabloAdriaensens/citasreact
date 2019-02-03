3f80ce8b563845d7eac3e045e9d6017c80e328cd
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-03 18:45:32 +0100

    Creamos el Header y lo enlazamos con el padre

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Header from "./componentes/header";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header
+                    titulo = {'Administrador de Pacientes de Veterinaria'}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
